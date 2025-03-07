19ffa0ffea02c04bf74288fa223f84f2ded37440
hmhmchm <hyeonminchoi5@gmail.com>
2025-03-07 10:45:25 +0900

    ♻️refactor : 메서드 구조 변경

--- backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
@@ -3,8 +3,14 @@ package com.snackoverflow.toolgether.domain.post.service;
 import com.snackoverflow.toolgether.domain.post.dto.PostCreateRequest;
 import com.snackoverflow.toolgether.domain.post.dto.PostResponse;
 import com.snackoverflow.toolgether.domain.post.dto.PostUpdateRequest;
+import com.snackoverflow.toolgether.domain.post.entity.Post;
+import com.snackoverflow.toolgether.domain.post.repository.PostRepository;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public interface PostService {
     PostResponse createPost(PostCreateRequest request);
 
@@ -13,4 +19,6 @@ public interface PostService {
     void deletePost(Long postId);
 
     PostResponse updatePost(Long postId, @Valid PostUpdateRequest request);
+
+    Post findPostById(Long aLong); // 예약에 필요한 메서드
 }
