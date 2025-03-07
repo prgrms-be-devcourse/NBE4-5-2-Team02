09f8ca11dfc18fd4beadf27f6e6065508d22dd8d
hmhmchm <hyeonminchoi5@gmail.com>
2025-03-07 12:20:20 +0900

    ♻️refactor : 인터페이스 @Service 어노테이션 제거

--- backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
@@ -4,13 +4,8 @@ import com.snackoverflow.toolgether.domain.post.dto.PostCreateRequest;
 import com.snackoverflow.toolgether.domain.post.dto.PostResponse;
 import com.snackoverflow.toolgether.domain.post.dto.PostUpdateRequest;
 import com.snackoverflow.toolgether.domain.post.entity.Post;
-import com.snackoverflow.toolgether.domain.post.repository.PostRepository;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-@Service
 public interface PostService {
     PostResponse createPost(PostCreateRequest request);
 
