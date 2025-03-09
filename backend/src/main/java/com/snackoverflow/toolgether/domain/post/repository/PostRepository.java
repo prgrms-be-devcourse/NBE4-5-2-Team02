8875b8bf2dbb1a62ac7796ed5927f312c475d2fb
hmhmchm <hyeonminchoi5@gmail.com>
2025-03-09 21:43:02 +0900

    ✨feat : 검색 필터링 조회 API 추가

--- backend/src/main/java/com/snackoverflow/toolgether/domain/post/repository/PostRepository.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/post/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.snackoverflow.toolgether.domain.post.repository;
 
 import com.snackoverflow.toolgether.domain.post.entity.Post;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+@Repository
+@Primary
+public interface PostRepository extends JpaRepository<Post, Long> , PostRepositoryCustom{
 
 }
--- backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
+++ backend/src/main/java/com/snackoverflow/toolgether/domain/post/service/PostService.java
@@ -2,9 +2,12 @@ package com.snackoverflow.toolgether.domain.post.service;
 
 import com.snackoverflow.toolgether.domain.post.dto.PostCreateRequest;
 import com.snackoverflow.toolgether.domain.post.dto.PostResponse;
+import com.snackoverflow.toolgether.domain.post.dto.PostSearchRequest;
 import com.snackoverflow.toolgether.domain.post.dto.PostUpdateRequest;
 import com.snackoverflow.toolgether.domain.post.entity.Post;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PostService {
     PostResponse createPost(PostCreateRequest request);
@@ -16,4 +19,6 @@ public interface PostService {
     PostResponse updatePost(Long postId, @Valid PostUpdateRequest request);
 
     Post findPostById(Long aLong); // 예약에 필요한 메서드
+
+    Page<PostResponse> searchPosts(PostSearchRequest request, Pageable pageable);
 }
